ffb580a2927f0833c05a7b5de4ef54872b57aee1
super157310 <super157310@gmail.com>
2026-05-06 10:26:22 +0900

    d

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'student.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${3000}`);
 });
